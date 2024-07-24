3cb4e68054eef90710a7ccc9cd08f659f3d09823
luthfi <lurrah30@gmail.com>
2024-07-24 23:35:22 +0700

    Update StoreContext.jsx

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://food-app-m1ys7yh5f-luthfis-projects-6cefe16f.vercel.app";
+  const url = "https://food-app-api-eight.vercel.app";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
