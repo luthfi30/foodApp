99b2df7788149722d088999aa353854f5f8167e9
luthfi30 <lurrah30@gmail.com>
2024-12-18 19:27:05 +0700

    fix button position

--- client/src/components/FoodItem/FoodItem.jsx
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -8,22 +8,27 @@ const FoodItem = ({ id, name, price, description, image }) => {
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        {!cartItems?.[id] ? (
-          <img className="add" onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
-        ) : (
-          <div className="food-item-counter">
-            <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
-            <p>{cartItems[id]}</p>
-            <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
-          </div>
-        )}
-      </div>
-      <img className="food-item-image" src={image} alt="" />
-      <div className="food-item-info">
-        <div className="food-item-name-rating">
-          <p>{name}</p>
-        </div>
+        {/* Button Add di kanan atas */}
+        <img className="add-btn-top-right" onClick={() => addToCart(id)} src={assets.add_icon_white} alt="Add Icon" />
 
+        {/* Gambar Utama */}
+        <img className="food-item-image" src={image} alt={name} />
+
+        {/* Button Add di bawah gambar */}
+        <div className="add-btn-bottom">
+          {!cartItems?.[id] ? (
+            <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="Add Icon" />
+          ) : (
+            <div className="food-item-counter">
+              <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="Remove" />
+              <p>{cartItems[id]}</p>
+              <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="Add" />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="food-item-info">
+        <p className="food-item-name">{name}</p>
         <p className="food-item-description">{description}</p>
         <p className="food-item-price">{rupiah(price)}</p>
       </div>
