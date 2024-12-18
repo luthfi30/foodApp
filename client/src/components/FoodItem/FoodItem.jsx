158bb90ed2ad5f38e27ba210aa8cb5c8c237dab7
luthfi30 <lurrah30@gmail.com>
2024-12-18 19:28:41 +0700

    still

--- client/src/components/FoodItem/FoodItem.jsx
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -8,27 +8,23 @@ const FoodItem = ({ id, name, price, description, image }) => {
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        {/* Button Add di kanan atas */}
-        <img className="add-btn-top-right" onClick={() => addToCart(id)} src={assets.add_icon_white} alt="Add Icon" />
-
-        {/* Gambar Utama */}
-        <img className="food-item-image" src={image} alt={name} />
-
-        {/* Button Add di bawah gambar */}
-        <div className="add-btn-bottom">
-          {!cartItems?.[id] ? (
-            <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="Add Icon" />
-          ) : (
-            <div className="food-item-counter">
-              <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="Remove" />
-              <p>{cartItems[id]}</p>
-              <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="Add" />
-            </div>
-          )}
-        </div>
+        <img className="food-item-image" src={image} alt="" />
+        {!cartItems?.[id] ? (
+          <img className="add" onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
+        ) : (
+          <div className="food-item-counter">
+            <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+            <p>{cartItems[id]}</p>
+            <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
+          </div>
+        )}
       </div>
+
       <div className="food-item-info">
-        <p className="food-item-name">{name}</p>
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+        </div>
+
         <p className="food-item-description">{description}</p>
         <p className="food-item-price">{rupiah(price)}</p>
       </div>
